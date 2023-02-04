b389d06c8f6fe73c1b6b6bc75ce859dcd35aa2d9
Leon Martin <lema@work>
2023-02-04 20:43:20 +0100

    implementation progress

--- Milestone03/AccountsManager.cs
+++ Milestone03/AccountsManager.cs
@@ -14,7 +14,7 @@ class AccountsManager {
     {
         List<Account> userAccounts = new List<Account>();
 
-        Account accountA = new Account("123458790");
+        Account accountA = new Account("1234567890");
         accountA.MakeTransaction(34545.23m, "1222333442");
         accountA.MakeTransaction(-23235.23m, "4562333442");
         accountA.MakeTransaction(-69.77m, "1222333442");
--- Milestone03/UserInterface.cs
+++ Milestone03/UserInterface.cs
@@ -5,7 +5,7 @@ class UserInterface {
     public AccountsManager Manager;
 
     public string initMessage = """
-        Please select one option by entering the according number:
+        Menu options:
         (1) Display Balance Overview.
         (2) Display Account Details.
         (3) Make a Transaction.
@@ -23,7 +23,7 @@ class UserInterface {
 
         foreach (Account account in accounts)
         {
-            Console.WriteLine($"Account {account.AccountNumber}: {account.Balance}€ after {account.Transactions.Count} transactions.");
+            Console.WriteLine($"Account {account.AccountNumber}: {account.Balance} Euro after {account.Transactions.Count} transactions.");
         }
     }
 
@@ -35,8 +35,23 @@ class UserInterface {
         {
             if (account.AccountNumber == accountNumber)
             {
-                Console.WriteLine(account);
+                Console.WriteLine($"Account {account.AccountNumber}: {account.Balance} Euro after {account.Transactions.Count} transactions.");
                 return;
+            }
+        }
+
+        Console.WriteLine("No account with the specified number was found.");
+    }
+
+    public void SubmitTransaction(string accountNumber, decimal amount, string otherAccountNumber)
+    {
+        List<Account> accounts = Manager.Accounts;
+
+        foreach (Account account in accounts)
+        {
+            if (account.AccountNumber == accountNumber)
+            {
+                account.MakeTransaction(amount, otherAccountNumber);
             }
         }
 
@@ -64,7 +79,14 @@ class UserInterface {
 
         while (true)
         {
-            Console.WriteLine(initMessage);
+            Console.Write("""
+                Menu options:
+                (1) Display Balance Overview.
+                (2) Display Account Details.
+                (3) Make a Transaction.
+                (4) Exit.
+                Please enter a number: 
+                """);
 
             string chosenOption = Console.ReadLine() ?? "1";
             int option = Int32.Parse(chosenOption);
@@ -76,13 +98,24 @@ class UserInterface {
                     break;
 
                 case 2:
-                    Console.WriteLine("Please enter the account number:");
+                    Console.Write("Please enter the account number: ");
                     string accountNumber = Console.ReadLine() ?? "1";
 
                     DisplayAccountDetails(accountNumber);
                     break;
 
                 case 3:
+                    Console.Write("Please enter the account number from which the amount is to be transferred: ");
+                    string thisAccountNumber = Console.ReadLine() ?? "1";
+
+                    Console.Write("Please enter the amount to be transferred: ");
+                    string userInput = Console.ReadLine() ?? "0m";
+                    decimal amount = Decimal.Parse(userInput);
+
+                    Console.Write("Please enter the account number to which the amount is to be transferred: ");
+                    string otherAccountNumber = Console.ReadLine() ?? "1";
+
+                    SubmitTransaction(thisAccountNumber, amount, otherAccountNumber);
                     break;
 
                 case 4:
