5b33489e80399864f13d5897e343b9df1087b264
Leon Martin <Leon@Home>
2023-02-06 11:21:11 +0100

    quicksave

---
+++ Milestone01/solutions/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hm, doesn't look that difficult.");
+
+// Task 02.1: Working with Different Data Types
+
+// Exercise 1
+// Print a sentence on the console that states what your favorite animals are.
+
+Console.WriteLine("Owls are my favorite animals.");
+
+// Exercise 2
+// Store the string from Exercise 1 in a variable and print the variable on the console.
+
+string favoriteAnimalString = "Owls are my favorite animals.";
+
+Console.WriteLine(favoriteAnimalString);
+
+// Exercise 3
+// Store the string from Exercise 1 in a variable and use a string method to change every character within to upper case.
+// Print the variable on the console afterwards.
+
+string favoriteAnimalStringTwo = "Owls are my favorite animals.";
+favoriteAnimalStringTwo = favoriteAnimalStringTwo.ToUpper();
+
+Console.WriteLine(favoriteAnimalStringTwo);
+
+// Exercise 3
+// Store the string from Exercise 1 in a variable
+// Print the variable on the console afterwards.
+
+string favoriteAnimalStringThree = "Owls are my favorite animals.";
+favoriteAnimalStringThree = favoriteAnimalStringThree.ToUpper();
+
+Console.WriteLine(favoriteAnimalStringThree);
+
+// Exercise 1
+// Write a statement that prints your name on the console
+
+// Task 02.2: Loops and if-then-else Constructs
+
+// Task 02.3: Creating your own Classes and Objects
+
+// Task 02.4: Processing User Input
